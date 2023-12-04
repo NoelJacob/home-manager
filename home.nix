a109746b86a56d098395913bbed694212e2dda59
Noel Jacob <noeljacob91@gmail.com>
2023-12-04 12:54:44 +0530

    z

--- home.nix
+++ home.nix
@@ -1,72 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
 
   home.username = "john";
   home.homeDirectory = "/home/john";
+  home.stateVersion = "23.11";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.packages = with pkgs; [
+    hello
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
     ".gitconfig".text = ''
-[credential "https://github.com"]
-	helper = !/home/john/.nix-profile/bin/gh auth git-credential
-[credential "https://gist.github.com"] 
-	helper = !/home/john/.nix-profile/bin/gh auth git-credential
-[user]
-	email = noeljacob91@gmail.com
-	name = Noel Jacob
+        [credential "https://github.com"]
+            helper = !${config.home.homeDirectory}/.nix-profile/bin/gh auth git-credential
+        [credential "https://gist.github.com"]
+            helper = !${config.home.homeDirectory}/.nix-profile/bin/gh auth git-credential
+        [user]
+            email = noeljacob91@gmail.com
+            name = Noel Jacob
     '';
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-    ".local/bin/hms" = {
-      executable = true;
-      source = "${config.home.homeDirectory}/home-manager/hms";
+    ".local/bin/hm" = {
+        executable = true;
+        source = "${config.home.homeDirectory}/home-manager/hm";
     };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
+
   #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   #
   # or
@@ -77,7 +39,6 @@
     EDITOR = "micro";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager = {
     enable = true;
 #     path = "$HOME/home-manager";
@@ -104,7 +65,9 @@
       extra-nix-path = "nixpkgs=flake:nixpkgs";
     };
   };
+
   nixpkgs.config.allowUnfree = true;
+
   home.sessionPath = [
     "${config.home.homeDirectory}/.local/bin"
   ];
